b6b1d131c85cb99b32366472bbadd35e2517bcfe
KulSmuf <49077302+KulSmuf@users.noreply.github.com>
2020-06-03 01:41:51 +0200

    Add files via upload

--- blog/models.py
+++ blog/models.py
@@ -3,46 +3,31 @@ from django.utils import timezone
 from django.urls import reverse
 from django.contrib.auth.models import User
 
-
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
+    pub_date = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-
+    value = models.CharField(max_length=100, default=title)
+    
     def __str__(self):
         return self.title
-
+    
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
-
-
+    
 class University(models.Model):
+    
+    types = [(1, 'Politechnika'),
+             (2, 'Uniwersytet Ogólny'),
+             (3, 'Uniwersytet Medyczny'),
+             (4, 'Uniwersytet Ekonomiczny'),
+             (5, 'Uniwersytet Przyrodniczy')]
+    
     name = models.CharField(max_length=100)
-    description = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
-    location = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
-
-
-class Course(models.Model):
-    name = models.CharField(max_length=100)
-    university = models.ForeignKey(University, on_delete=models.CASCADE)
-    date_posted = models.DateTimeField(default=timezone.now)
-    description = models.TextField()
-    grade = models.IntegerField()
-    semesters = models.IntegerField()
-    department = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
+    city = models.CharField(max_length=100)
+    type_1 = models.CharField(max_length=100, choices=types)
+    national_ranking = models.IntegerField()
+    website_url = models.URLField()
