0c7dda67ce64310dcc6d4776f100f19d58a74ee1
KulSmuf <49077302+KulSmuf@users.noreply.github.com>
2020-06-06 19:31:05 +0200

    poprawki

--- search/filters.py
+++ search/filters.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import django_filters
-
+from django import forms
+from django_filters import ChoiceFilter
 from blog.models import Course, University
 
 class CourseFilter(django_filters.FilterSet):
@@ -10,15 +11,35 @@ class CourseFilter(django_filters.FilterSet):
             ('descending', 'Descending')
             )
     
-    ordering = django_filters.ChoiceFilter(label='Ordering', choices=CHOICES, method='filter_by_order')
+    #ordering = django_filters.ChoiceFilter(label='Ordering', choices=CHOICES, method='filter_by_order')
+    #name = django_filters.CharFilter(lookup_expr='icontains')
     
     class Meta:
         model = Course
-        fields = {'name', 'university', 'type_1', 'type_2'}
+        fields = {'nazwa', 'uniwersytet', 'tryb', 'tytul'}
         
     def filter_by_order(self, queryset, name, value):
         expression = 'pub_date' if value == 'ascending' else '-pub_date'
         return queryset.order_by(expression)
+    
+class UniversityFilter(django_filters.FilterSet):
         
+    
+    CHOICES = (
+            ('rosnaco', 'Rosnaco'),
+            ('malejaco', 'Malejaco')
+            )
+    
+    ordering = django_filters.ChoiceFilter(label='Ranking', choices=CHOICES, method='filter_by_order')
+    #name = django_filters.CharFilter(lookup_expr='icontains')
+    
+    class Meta:
+        model = University
+        fields = {'nazwa', 'lokacja', 'typ'}
+        
+    def filter_by_order(self, queryset, name, value):
+        expression = 'national_ranking' if value == 'rosnaco' else '-national_ranking'
+        return queryset.order_by(expression)
+    
         
         
--- search/views.py
+++ search/views.py
@@ -1,32 +1,36 @@
 from django.shortcuts import render
-from blog.models import Course
+from blog.models import Course, Post, University
 import json
 from django.http import HttpResponse
 from django.views.generic import ListView
 
-from .filters import CourseFilter
+from .filters import CourseFilter, UniversityFilter
 
-
-# from .documents import PostDocument
+#from .documents import PostDocument
 # Create your views here.
 
 def main_search(request):
+    
     return render(request, 'search/main_search.html')
 
 
-def search(request):
-    # posts = Post.objects.all()
-    posts = {'value': 111}
-
-    return render(request, 'search/search_autocomplete.html', {'posts': posts})
-
-
-class CourseListView(ListView):
-    model = Course
-    template_name = 'blog/home.html'
-
+class UniversityListView(ListView):
+    
+    model = University
+    template_name = 'blog/unis.html' 
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['filter'] = CourseFilter(self.request.GET,
-                                         queryset=self.get_queryset())
+        context['filter'] = UniversityFilter(self.request.GET, queryset=self.get_queryset())
+        return context
+
+    
+class CourseListView(ListView):
+    
+    model = Course
+    template_name = 'blog/home.html' 
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = CourseFilter(self.request.GET, queryset=self.get_queryset())
         return context
